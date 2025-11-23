495c713f24b38bde552ce8d9fde5754f92d54745
CodeEngina2027 <sahileegaurkar541@gmail.com>
2025-11-23 19:28:14 +0530

    Refactor server.js for clarity and organization
    Refactor server.js to improve code clarity and organization.

--- server/server.js
+++ server/server.js
@@ -5,21 +5,24 @@ import connectDB from './config/mongodb.js'
 import userRouter from './routes/userRoutes.js'
 import imageRouter from './routes/imageRoutes.js'
 
-
-
-const PORT=process.env.PORT || 4000  //if the port no. is available in the env then it will used the port no. from the env otherwise it will used 4000
-const app=express()
-//after we have to add the cors in the express app 
+const PORT = process.env.PORT || 4000
+const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-app.use(cors())
-await connectDB() //when we call this function it will connect our express app to our mongodb database
-//await shows wait until the mongodb connection is fully established 
+app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/user',userRouter) 
-app.use('/api/image',imageRouter)
+app.use(cors({
+    origin: "https://imagify-1-wm7a.onrender.com", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}))
 
-app.get('/',(req,res)=>res.send("API Working "))
+await connectDB()
 
-app.listen(PORT,()=>console.log('Server running on port'+ PORT));
+app.use('/api/user', userRouter)
+app.use('/api/image', imageRouter)
+
+app.get('/', (req, res) => res.send("API Working"))
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT))
