0313d4e41a5f5c86897093ac841db24bf06a7bf7
sahileegaurkar541@gmail.com <sahileegaurkar541@gmail.com>
2025-12-12 22:48:47 +0530

    fix .env comment issue

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     origin: [
         "http://localhost:5173",                 // Local frontend (dev)
-        "https://imagify-2-lwsr.onrender.com"   // Render frontend (live)
+        "https://imagify-2-lwsr.onrender.com" ,  // Render frontend (live)
         "https://imagify-4-qejm.onrender.com"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
